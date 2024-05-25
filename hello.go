cfbf931bd5a9e238764f83ab1cba159376586e20
sif7 <sif7@outlook.com>
2024-05-25 17:57:26 +0100

    comments

--- hello.go
+++ hello.go
@@ -1,8 +1,12 @@
 // Go doesn't need a virtual environment like Python. Instead, the Go module system manages dependencies
+
 // Go versions: https://go.dev/dl/
 // Go download: cd Downloads; wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
 // Go extract: sudo tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
 // Go add to PATH: export PATH=$PATH:/usr/local/go/bin
+
+// Opposite terminology: Python has packages (dir) containing modules (.py files)
+// Opposite terminology: Go modules is a collection of packages that are versioned together
 
 package main
 
@@ -16,3 +20,4 @@ func main() {
 	// snippet "fmp"
 	fmt.Print("Hello World withoutautomatic newline\n")
 }
+
