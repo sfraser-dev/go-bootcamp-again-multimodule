109fecde6161ba6c439264a2617f9de710a63edb
Jack Edge <js77js@yahoo.co.uk>
2024-06-02 18:51:23 +0100

    adding cards dir

---
+++ cards/deck.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+// new type called deck of type sting slice
+type deck []string
+
+// receiver: "extend" the functionality of the deck type via receiver
+func (d deck) printWholeDeck() {
+	for _, card := range d {
+		fmt.Printf("%25s\n", card)
+	}
+}
+
+// receiver
+func (d deck) shuffleDeck() {
+	rand.Shuffle(len(d), func(i, j int) {
+		d[i], d[j] = d[j], d[i]
+	})
+}
+
+// receiver
+func (d deck) writeToFile() {
+	err := os.WriteFile("myfile.txt", []byte(toSingleString(d)), 0755)
+	if err != nil {
+		fmt.Println("error writing to file")
+	}
+}
+
+// receiver
+func (d deck) readFromFile() {
+	var bs = []byte{}
+	bs, err := os.ReadFile("myfile.txt")
+	if err != nil {
+		fmt.Println("error reading from file")
+	}
+	fmt.Println(fromSingleString(string(bs), ":"))
+}
+
+// helper functions
+func newDeck() deck {
+	cards := deck{}
+	// creating two LISTS, suits and numbers
+	suits := [4]string{"Clubs", "Diamonds", "Hearts", "Spades"}
+	numbers := [13]string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+	for _, suit := range suits {
+		for _, number := range numbers {
+			tempStr := number + " of " + suit
+			cards = append(cards, tempStr)
+		}
+	}
+	return cards
+}
+
+func deal(d deck, handSize int) (hand deck, remainingCards deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func getOneCard() string {
+	return "Ace of Spades"
+}
+
+func toSingleString(d deck) string {
+	var s string = ""
+	for _, card := range d {
+		s = s + card + ":"
+	}
+	return s
+}
+
+func fromSingleString(s string, sep string) (d deck) {
+	d = strings.Split(s, sep)
+	return d
+}
--- cards/main.go
+++ cards/main.go
@@ -1,26 +1,65 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	// var card string = "ace of spades" 	// formal declaration
 	//card := "ace of spades" 				// type inference declaration
-	card := newCard()
-	fmt.Printf("card = %s; card=%q\n", card, card)   // string, double quoted str. snippet "fmpf"
-	cards_wee := []string{"Two of Clubs", newCard()} // slice of strings
-	cards := append(cards_wee, "Five of Hearts")     // argument cards_wee not changed
+	/*
+		card := getOneCard()
+		fmt.Printf("card = %s; card=%q\n", card, card) // string, double quoted str. snippet "fmpf"
+		cardsWee := deck{"Two of Clubs", getOneCard()}    // deck is type slice of string
+		cards := append(cardsWee, "Five of Hearts")    // argument cards_wee not changed
+	*/
+	/*
+		// for loop
+		for i := 0; i < len(cards); i += 1 {
+		 	fmt.Println(cards[i]) // snippet "fp" or "fmpl"
+		 }
+	*/
+	/*
+		// for loop range keyword
+		for i, card := range cards { // range is a keyword in Go, returns arr/slice ind and copy of value
+			fmt.Printf("i = %3d, card = %15s", i, card)
+			fmt.Println("")
+		}
+	*/
 
-	// for i := 0; i < len(cards); i += 1 {
-	// 	fmt.Println(cards[i]) // snippet "fp" or "fmpl"
-	// }
+	var cards deck = newDeck()            // new deck of cards
+	fmt.Println("\nprint the whole deck") // receiver functions are LIKE "OOP" in Go
+	cards.printWholeDeck()
+	cards.shuffleDeck() // shuffle the deck
+	fmt.Println("\nprint the shuffled deck")
+	cards.printWholeDeck()
 
-	// range keyword
-	for i, card := range cards { // range is a keyword in Go, returns arr/slice ind and copy of value
-		fmt.Printf("i = %3d, card = %15s", i, card)
-		fmt.Println("")
-	}
-}
+	fmt.Println("\ndeal two hands of 5 cards")
+	hand1, remainingCards := deal(cards, 5)
+	fmt.Println("hand1:")
+	hand1.printWholeDeck()
+	fmt.Println("hand2:")
+	hand2, remainingCards := deal(remainingCards, 5)
+	hand2.printWholeDeck()
 
-func newCard() string {
-	return "Ace of Spades"
+	fmt.Println("\nbyte slice interlude")
+	hiStr := "Hi there"
+	bs := []byte(hiStr)
+	fmt.Println(hiStr) // hi there
+	fmt.Println(bs)    // [72 105 32 116 104 101 114 101]
+
+	fmt.Println("\nhand 1 to single string")
+	h1 := toSingleString(hand1)
+	fmt.Println(h1)
+
+	fmt.Println("\nhand 1 reconstructed to string slice")
+	hand11 := fromSingleString(h1, ":")
+	hand11.printWholeDeck()
+
+	fmt.Println("\nwrite the whole deck to file")
+	cards.writeToFile()
+
+	fmt.Println("\nread the whole deck from file")
+	cards.readFromFile()
+	cards.printWholeDeck()
 }
