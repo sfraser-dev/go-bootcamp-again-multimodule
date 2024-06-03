aff6c4306c3f5997dbdb23800a159fb519faca37
Jack Edge <js77js@yahoo.co.uk>
2024-06-03 14:13:40 +0100

    finishing off odd_even exercise

--- odd_even/reusable/odd_even.go
+++ odd_even/reusable/odd_even.go
@@ -7,10 +7,20 @@ import (
 
 func OddEven() {
 	var mySlice = []int{}
+	var myResults = []string{}
 
 	fmt.Println("\nOddEven")
 	for i := 0; i < 10; i += 1 {
 		mySlice = append(mySlice, rand.Intn(100))
 		fmt.Printf("mySlice = %v\n", mySlice)
 	}
+	for i := 0; i < 10; i += 1 {
+		if mySlice[i]%2 == 0 {
+			myResults = append(myResults, "E")
+		} else {
+			myResults = append(myResults, "O")
+		}
+	}
+	fmt.Printf("myResults = %v\n", myResults)
+
 }
